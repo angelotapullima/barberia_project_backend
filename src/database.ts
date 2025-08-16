b552bf70ffc20dcf4a96a97e22559f68e3314c7d
Angelo <angelotapullima@credicorpcapital.com>
2025-08-16 06:04:24 -0500

    fix dayJS

--- src/database.ts
+++ src/database.ts
@@ -3,13 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '********' : 'undefined'); // Mask password
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   ssl: {
